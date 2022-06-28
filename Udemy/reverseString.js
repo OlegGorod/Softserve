d1056393d3bb303e92115826a2084d47b9c4b54e
Oleg <franky.ua@gmail.com>
2022-06-28 17:22:54 +0300

    upgrade file

--- Udemy/reverseString.js
+++ Udemy/reverseString.js
@@ -1,14 +1,30 @@
 const someString = 'This is some strange string';
 
-function reverse(string) {
-    if (typeof string !== "string") {
-        return 'error';
-    } else {
-        let arrayString = string.split('');
-        let reverseArray = arrayString.reverse();
-        let reverseString = reverseArray.join('');
-        return reverseString ;  
-    } 
-}
+console.log(someString.split('').reverse().join(''));
 
-reverse(someString);
+// function reverse(string) {
+//     if (typeof string !== "string") {
+//         return 'error';
+//     } else {
+//         let arrayString = string.split('');
+//         let reverseArray = arrayString.reverse();
+//         let reverseString = reverseArray.join('');
+//         return reverseString ;  
+//     } 
+// }
+
+// reverse(someString);
+
+
+// function reverse(str) {
+//     if (typeof(str) !== 'string') {
+//         return "Ошибка!";
+//     }
+//     let newStr = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         newStr += str[i];
+//     }
+//     return newStr;
+// }
+
+// reverse(someString);
